23af423e8d1c526f5ff22f3792722e0c2b30b9ee
xiaolei <lei.xiao@ifchange.com>
2018-09-19 11:21:37 +0800

    modify reactapp

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,30 +1,20 @@
 'use strict';
 const api = require('./generalConfig/api');
-// const EasyWebpack = require('easywebpack-react');
-
-const path = require('path');
 const requestConfig = require('./generalConfig/requstConfig');
 module.exports = appInfo => {
     const config = exports = {};
     config.security = {
         csrf: {
-            enable: false,
             // 判断是否需要 ignore 的方法，请求上下文 context 作为第一个参数
             ignore: '/api',
-            ignoreJSON: true,
         },
-        domainWhiteList: [ 'http://localhost:3000' ],
-        credentials:true
-
-
     };
     // use for cookie sign key, should change to your own and keep security
     config.keys = appInfo.name + '_1531993216598_9019';
 
     // add your config here
-    //处理借口返回错误
     config.middleware = ['errorHandler'];
-    config.errorHandler = {
+    config.errorHandler={
         match: '/api',
     };
     //config.middleware = ['gzip'];
@@ -37,24 +27,17 @@ module.exports = appInfo => {
         mapping: {
             '.tpl': 'nunjucks',
         },
-    };
-    config.apiProxy = {
-        fetchUrl: '',
-        headers: {}
+        // defaultViewEngine: 'twig',
+        // mapping: {
+        //     // '.tpl': 'nunjucks',
+        //     '.twig': 'twig',
+        // },
     };
     config.api = api;
     config.bodyParser = {
         jsonLimit: '1mb',
         formLimit: '1mb',
     };
-    config.static = {
-        prefix:'/public/',
-        dir: path.join(appInfo.baseDir, 'app/reactWeb/build/')
-    };
-    // config.static = {
-    //     prefix:'/img/',
-    //     dir: path.join(appInfo.baseDir, 'app/reactWeb/build/img')
-    // };
     config.httpclient = requestConfig;
     return config;
 };
